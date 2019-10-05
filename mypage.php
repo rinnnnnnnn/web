d0858ee7c000b01dd055ee79f2016320806aedb2
rinnnnnnnn <31400563+rinnnnnnnn@users.noreply.github.com>
2019-10-05 20:37:38 +0900

    Update mypage.php

--- mypage.php
+++ mypage.php
@@ -89,10 +89,7 @@ p{
 </div>
 
 <?php
-$dsn = 'mysql:dbname=tb210282db; host=localhost';
-$user = 'tb-210282';
-$password = 'BGHZyT7Gvh';
-$pdo = new PDO($dsn, $user, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING));
+//データベースとの接続
 
 if (!session_id()) {
     session_start();
